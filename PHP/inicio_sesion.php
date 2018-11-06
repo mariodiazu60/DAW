a8c007b8a332191bced00f059111b21c7069ffd8
Toni Hernández Velasco <toni.herve309@hotmail.com>
2018-11-06 17:43:51 +0100

    nisman

--- PHP/inicio_sesion.php
+++ PHP/inicio_sesion.php
@@ -22,19 +22,21 @@
 
 		<section class="formularios">
 			<h2>Inicio de sesi&oacute;n:</h2>
+			<p>
 			<?php
 				if (isset($_COOKIE['usuario_recordado'])) {
 			    	$valores = explode(" ", $_COOKIE['usuario_recordado']);
-			    	echo "<p>Hola ".$valores[0].", su última visita fue el ".$valores[2]." a las ".$valores[3].".</p>";
+			    	echo "Hola ".$valores[0].", su última visita fue el ".$valores[2]." a las ".$valores[3].".<br><br>";
 			    	echo "<a href='control_salida.php'>Salir</a>";
 			    } elseif (isset($_SESSION['usuario_sesion'])) {
 			    	$valores = explode(" ", $_SESSION['usuario_sesion']);
-			    	echo "<p>Hola ".$valores[0].", su última visita fue el ".$valores[2]." a las ".$valores[3].".</p>";
+			    	echo "Hola ".$valores[0].", su última visita fue el ".$valores[2]." a las ".$valores[3].".<br><br>";
 			    	echo "<a href='control_salida.php'>Salir</a>";
 			    } else {
 			    	require_once("../Plantilla/login.inc");
 			    }
 			?>
+			</p>
 		</section>
 <?php
     require_once("../Plantilla/pie.inc");
